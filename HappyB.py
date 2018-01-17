4e94afe3e60c44266eca3782e8768bad2322f126
Dondry72 <muvhangof97@gmail.com>
2018-01-18 00:19:52 +0200

    added HappyB.py

---
+++ HappyB.py
@@ -0,0 +1,10 @@
+def HappyBirthDay():
+    print("Happy birth day to you")
+    print("Happy birth day to you")
+    print("Happy birth day, dear Dondry")
+    print("Happy birth day to you \n ")
+
+
+HappyBirthDay()
+
+print (" May you have a fantastic birthday....")
